e71ab0e9863fb7da25baa12165529e762abd75f0
Ricardo Siles <ricardosilesrivero@gmail.com>
2025-08-09 03:48:39 +0000

    https://www.redpat.tv/proxylpz/index.m3u8
    Coloca este en pat santa ceuz

--- src/lib/channels/pat.ts
+++ src/lib/channels/pat.ts
@@ -4,7 +4,7 @@ export const patChannels: Channel[] = [
   {
     id: 'pat-scz',
     name: 'PAT Santa Cruz',
-    url: 'https://w8.redpat.tv:8000/play/110/30354384.m3u8',
+    url: 'https://www.redpat.tv/proxylpz/index.m3u8',
   },
   {
     id: 'pat-lpz',
