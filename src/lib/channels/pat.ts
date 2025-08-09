5ae2575c7dee6d2b1180e40b2bd5b4d9f12e9b9d
Ricardo Siles <ricardosilesrivero@gmail.com>
2025-08-09 03:53:12 +0000

    cambia la url de el canal la pat santa cruz, muevelo a pat la paz y la u

--- src/lib/channels/pat.ts
+++ src/lib/channels/pat.ts
@@ -4,11 +4,11 @@ export const patChannels: Channel[] = [
   {
     id: 'pat-scz',
     name: 'PAT Santa Cruz',
-    url: 'https://www.redpat.tv/proxylpz/index.m3u8',
+    url: 'https://w8.redpat.tv:8000/play/110/16112403.m3u8',
   },
   {
     id: 'pat-lpz',
     name: 'PAT La Paz',
-    url: 'https://streamer01.digital.com.bo/session/a5da4006-74d2-11f0-9d9d-00505683ffaa$h1.0$default/2ap337/__cl/cg:sworigin2/__c/PAT/__op/hls-default/__f/14/audio_esp/index.m3u8',
+    url: 'https://www.redpat.tv/proxylpz/index.m3u8',
   },
 ];
