57b8d72556cca39b16cae3c1a4fb84cd13e96a80
Ricardo Siles <ricardosilesrivero@gmail.com>
2025-08-10 18:46:48 +0000

    https://d2qsan2ut81n2k.cloudfront.net/live/20446f64-67d8-4100-8c4b-20a75

--- src/lib/channels/bolivision.ts
+++ src/lib/channels/bolivision.ts
@@ -1,7 +1,7 @@
 import type { Channel } from '../types';
 
 // URLs originales
-const bolivisionLpzHdUrl = 'https://d3ew2cr4djta2j.cloudfront.net/original.m3u8';
+const bolivisionLpzHdUrl = 'https://d2qsan2ut81n2k.cloudfront.net/live/20446f64-67d8-4100-8c4b-20a759a8e919/ts:abr.m3u8';
 const bolivisionSczHdUrl = 'https://d2qsan2ut81n2k.cloudfront.net/live/3338960e-86ca-4c50-a567-913c663b26fc/medialist_4276517416086298479_hls.m3u8';
 const bolivisionLpzSdUrl = 'https://d3ew2cr4djta2j.cloudfront.net/480p.m3u8';
 const bolivisionSczSdUrl = 'https://d2e599a9ekjlc5.cloudfront.net/480p.m3u8';
