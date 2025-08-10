b2d5558b75d85d47e83dc3c16872a3a16f40a442
Ricardo Siles <ricardosilesrivero@gmail.com>
2025-08-10 18:42:02 +0000

    en el vlc no me da ningun error ni restriccion pero aqui se queda cargan

--- src/lib/channels/atb.ts
+++ src/lib/channels/atb.ts
@@ -1,10 +1,12 @@
 import type { Channel } from '../types';
 
+const atbBoliviaUrl = 'https://live.eu-north-1a.cf.dmcdn.net/sec2(cdyUusYk2-R9gx_JHakyw0-XKONn39Qv1236Mi3UFhshiuIIlj40z8e3Y9WZ6-qrDzDqJbauVmQ-1yy55BDMhFYjlyFy-_fbDu156YsxVx7EekRMLEnijvLWevh4thJy)/dm/3/x84eirw/s/live-480.m3u8';
+
 export const atbChannels: Channel[] = [
   {
     id: 'atb-bolivia',
     name: 'ATB Bolivia',
-    url: 'https://live.eu-north-1a.cf.dmcdn.net/sec2(cdyUusYk2-R9gx_JHakyw0-XKONn39Qv1236Mi3UFhshiuIIlj40z8e3Y9WZ6-qrDzDqJbauVmQ-1yy55BDMhFYjlyFy-_fbDu156YsxVx7EekRMLEnijvLWevh4thJy)/dm/3/x84eirw/s/live-480.m3u8',
+    url: `/api/proxy?url=${encodeURIComponent(atbBoliviaUrl)}`,
   },
   {
     id: 'atb-movil',
